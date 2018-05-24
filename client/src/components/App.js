985ada3987fcab7bb20ddc75368af51f7f8109c5
kietnguyen <kietnguyen.me@gmail.com>
2018-05-24 13:49:38 +0700

    [ADD] structure client project

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        This is my app
+      </React.Fragment>
+    )
+  }
+}
+
+export default App;
